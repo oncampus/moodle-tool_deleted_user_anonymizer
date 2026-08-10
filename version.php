84bd1c1f3f1c9d92fad77a81f0a61793bcdf0f8a
Marius Rosenbaum <marius.rosenbaum@oncampus.de>
2026-08-10 13:32:47 +0000

    chore: bumped version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_deleted_user_anonymizer';
-$plugin->release = '4.5.1';
-$plugin->version = 2025121200;
+$plugin->release = '4.5.2';
+$plugin->version = 2025121201;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
